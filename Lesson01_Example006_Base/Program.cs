c5ef18ef8baf5270356a90fb53f7adf98311ed19
nailharr <nailharr@gmail.com>
2023-02-28 07:03:58 +0300

    find max simple method

--- Lesson01_Example006_Base/Program.cs
+++ Lesson01_Example006_Base/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 3;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("maximum -> ");
+Console.WriteLine(max);
