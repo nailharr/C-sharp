bf2c9c61b28619baec37160c3d6e65d3bab7923e
nailharr <nailharr@gmail.com>
2023-02-28 09:32:01 +0300

    modify func

--- Lesson02_Example008_IntroMethod/Program.cs
+++ Lesson02_Example008_IntroMethod/Program.cs
@@ -7,7 +7,7 @@
 }
 
 int a1 = 15;
-int b1 = 21;
+int b1 = 21156;
 int c1 = 39;
 int a2 = 1152;
 int b2 = 213;
@@ -16,10 +16,14 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 331;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3)
+    );
 
 // int max = a1;
 // if (b1 > max) max = b1;
