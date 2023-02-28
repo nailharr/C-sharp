a17a5680d475a8a461f96eb7276380d9057d95d9
nailharr <nailharr@gmail.com>
2023-02-28 06:00:01 +0300

    lesson1 upd ex04 div *random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,8 @@
-﻿double num1=12;
-double num2=5;
-Console.WriteLine(num1/num2);
+﻿// double num1 = 12;
+// double num2 = 5;
+// Console.WriteLine(num1 / num2);
+
+double num1 = new Random().Next(1, 100);
+double num2 = 5;
+Console.Write($"{num1} / {num2} -> ");
+Console.WriteLine(num1 / num2);
