2f5ce665b63c6d039214c1c48b956f47538c4316
nailharr <nailharr@gmail.com>
2023-02-28 06:21:53 +0300

    ex05 if userad

--- Example005_HelloVipUser/Program.cs
+++ Example005_HelloVipUser/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.WriteLine("Enter your username:");
+string username = Console.ReadLine();
+if (username == "Harry")
+    Console.WriteLine($"WOW! That's Harry!");
+else Console.WriteLine($"Hello, {username}");
