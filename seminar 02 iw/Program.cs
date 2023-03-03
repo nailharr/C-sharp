0f27feaa01affe5b398ddfeab106324984119267
nailharr <nailharr@gmail.com>
2023-03-03 13:09:14 +0300

    solved task 1 - biggest digit from random [10,99]

---
+++ seminar 02 iw/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// START // ————————————————————————————————————————————————————————————————————————
+
+int BiggestNumber(int number)
+{
+    int firstDigit = number % 10;
+    int secondDigit = number / 10;
+
+    if (firstDigit > secondDigit) return firstDigit;
+    else return secondDigit;
+}
+
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+Console.WriteLine(BiggestNumber(number));
+
+// ————————————————————————————————————————————————————————————————————————// END //
