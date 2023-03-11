5b0f2402d6c001dcc5b56cc95b127ba429c8812d
nailharr <nailharr@gmail.com>
2023-03-11 15:37:01 +0300

    add ? nullable parameter of input string

--- Lesson01_Example002_HelloUser/Program.cs
+++ Lesson01_Example002_HelloUser/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Enter your name:");
-string username = Console.ReadLine();
+string? username = Console.ReadLine();
 // Console.Write("Hello, ");
 // Console.Write(username + "!");
 Console.WriteLine($"Hello, {username}!");
