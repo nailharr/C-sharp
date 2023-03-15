b039d0e50778e0b64b8742f8db79082a2bbda729
nailharr <nailharr@gmail.com>
2023-03-15 13:25:40 +0300

    upd: add messages to parallel and same line graphs in task 43 — Point of cross y=kx+b

--- seminar 06 homework/Program.cs
+++ seminar 06 homework/Program.cs
@@ -5,60 +5,60 @@
 // 1, -7, 567, 89, 223-> 3
 
 
-int Request(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-// int[] FillArray(int size)
+// int Request(string message)
 // {
-//     int[] array = new int[size];
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write($"Enter number {i + 1}: ");
-//         array[i] = Convert.ToInt32(Console.ReadLine());
-//     }
-//     return array;
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
 // }
 
-int[] FillArray(int size) 
-{ 
-    int[] array = new int[size]; 
-    for (int i = 0; i < array.Length; i++) 
-    { 
-        Console.Write($"Enter number {i + 1}: "); 
-        
-        if (!int.TryParse(Console.ReadLine(), out int input))
-        {
-            Console.WriteLine("You must enter a integer. Please try again.");
-            i--;
-            continue;
-        }
-        
-        array[i] = input; 
-    } 
-    return array; 
-}
+// // int[] FillArray(int size)
+// // {
+// //     int[] array = new int[size];
+// //     for (int i = 0; i < array.Length; i++)
+// //     {
+// //         Console.Write($"Enter number {i + 1}: ");
+// //         array[i] = Convert.ToInt32(Console.ReadLine());
+// //     }
+// //     return array;
+// // }
 
-int CountOfPositiveNumbers(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] > 0)
-        {
-            count++;
-        }
-    }
-    return count;
-}
+// int[] FillArray(int size) 
+// { 
+//     int[] array = new int[size]; 
+//     for (int i = 0; i < array.Length; i++) 
+//     { 
+//         Console.Write($"Enter number {i + 1}: "); 
 
-int sizeArray = Request("Enter count of numbers: ");
-int[] array = FillArray(sizeArray);
-int result = CountOfPositiveNumbers(array);
+//         if (!int.TryParse(Console.ReadLine(), out int input))
+//         {
+//             Console.WriteLine("You must enter a integer. Please try again.");
+//             i--;
+//             continue;
+//         }
 
-Console.WriteLine($"You have entered {result} digits greather than zero.");
+//         array[i] = input; 
+//     } 
+//     return array; 
+// }
+
+// int CountOfPositiveNumbers(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// int sizeArray = Request("Enter count of numbers: ");
+// int[] array = FillArray(sizeArray);
+// int result = CountOfPositiveNumbers(array);
+
+// Console.WriteLine($"You have entered {result} digits greather than zero.");
 
 
 
@@ -67,23 +67,34 @@ Console.WriteLine($"You have entered {result} digits greather than zero.");
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-// void CrossPoint(int k1, int b1, int k2, int b2)
-// {
-//     double x = Convert.ToDouble(b2 - b1) / (k1 - k2);
-//     double y = k1 * x + b1;
-//     System.Console.WriteLine($"({x}; {y})");
-// }
+void CrossPoint(int k1, int b1, int k2, int b2)
+{
+    double x = Convert.ToDouble(b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    System.Console.WriteLine($"({x}; {y})");
+}
 
-// int InputData(string input)
-// {
-//     Console.Write($"Enter value {input}: ");
-//     return Convert.ToInt32(Console.ReadLine());
-// }
+int InputData(string input)
+{
+    Console.Write($"Enter value {input}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-// int k1Value = InputData("k1");
-// int b1Value = InputData("b1");
-// int k2Value = InputData("k2");
-// int b2Value = InputData("b2");
+int k1Value = InputData("k1");
+int b1Value = InputData("b1");
+int k2Value = InputData("k2");
+int b2Value = InputData("b2");
 
-// Console.Write($"Coordinates of cross point: ");
-// CrossPoint(k1Value, b1Value, k2Value, b2Value);
+Console.Write($"Coordinates of cross point: ");
+if (k1Value == k2Value && b1Value == b2Value)
+{
+    Console.WriteLine($"Graph on the same line");
+}
+else if (k1Value == k2Value)
+{
+    Console.WriteLine($"Graphs is parallel");
+}
+else
+{
+    CrossPoint(k1Value, b1Value, k2Value, b2Value);
+}
