924178d4cf4fac88981c56f64d49e608886a8a6a
nailharr <nailharr@gmail.com>
2023-03-19 21:32:49 +0300

    solved task 50 — value element from position 2D array

--- seminar 07 homework/Program.cs
+++ seminar 07 homework/Program.cs
@@ -5,63 +5,89 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-double[,] Create2DArray(int m, int n)
-{
-    double[,] empty2DArray = new double[m, n];
-    return empty2DArray;
-}
+// double[,] Create2DArray(int m, int n)
+// {
+//     double[,] empty2DArray = new double[m, n];
+//     return empty2DArray;
+// }
 
-double[,] Fill2DArray(double[,] newArray, int minValue, int maxValue)
-{
-    double[,] fill2DArray = new double[newArray.GetLength(0), newArray.GetLength(1)];
-    for (int i = 0; i < newArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < newArray.GetLength(1); j++)
-        {
-            fill2DArray[i, j] = Math.Round(
-                new Random().Next(minValue, maxValue + 1)
-                + new Random().NextDouble()
-                , 2);
-        }
-    }
-    return fill2DArray;
-}
+// double[,] Fill2DArray(double[,] newArray, int minValue, int maxValue)
+// {
+//     double[,] fill2DArray = new double[newArray.GetLength(0), newArray.GetLength(1)];
+//     for (int i = 0; i < newArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < newArray.GetLength(1); j++)
+//         {
+//             fill2DArray[i, j] = Math.Round(
+//                 new Random().Next(minValue, maxValue + 1)
+//                 + new Random().NextDouble()
+//                 , 2);
+//         }
+//     }
+//     return fill2DArray;
+// }
 
-void Show2DArray(double[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
+// void Show2DArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
 
-int rows = 5;
-int cols = 6;
-int minValue = -100;
-int maxValue = 100;
+// int rows = 5;
+// int cols = 5;
+// int minValue = -100;
+// int maxValue = 100;
 
-Show2DArray(Fill2DArray(Create2DArray(rows, cols), minValue, maxValue));
+// double[,] realNumArray = Fill2DArray(Create2DArray(rows, cols), minValue, maxValue);
 
+// Show2DArray(realNumArray);
 
-// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же указание,
+//  что такого элемента нет.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// 17 -> такого числа в массиве нет
+// 1,7 -> такого числа в массиве нет
 
+// В качестве входящего массива используется решение задачи 47.
 
+// void ValueOfMatrixElement(double[,] array, int indexRow, int indexColumn)
+// {
+//     if (indexRow < array.GetLength(0) && indexColumn < array.GetLength(1))
+//     {
+//         Console.WriteLine($"Value of selected element = {array[indexRow, indexColumn]}");
+//     }
+//     else
+//     {
+//         Console.WriteLine(":( This element not exist in the matrix.");
+//     }
+// }
 
-// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// int indexRow = 2;
+// int indexColumn = 4;
+
+// Console.WriteLine($"Row index of element = {indexRow}");
+// Console.WriteLine($"Column index of element = {indexColumn}");
+// ValueOfMatrixElement(realNumArray, indexRow, indexColumn);
+// Console.WriteLine();
+
+// ============================================================================
+// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+// ============================================================================
 
 
