353b6e0eec9608378fb8a4080eda2d2d2049e028
nailharr <nailharr@gmail.com>
2023-03-27 01:36:42 +0300

    solved task 68 — Akkerman

--- seminar 09 homework/Program.cs
+++ seminar 09 homework/Program.cs
@@ -44,7 +44,7 @@
 // }
 
 // int numberM = 8;
-// int numberN = 5;
+// int numberN = 4;
 
 // Console.WriteLine(SumNumbersFromMToN(numberM, numberN));
 
@@ -55,3 +55,12 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 // ----------------------------------------------------------------
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+Console.WriteLine(Akkerman(3, 2));
